3de1172bb389e31c2f5f8e07243e74a3853a7800
pB <webforeman@gmail.com>
2020-07-20 10:41:48 +0300

    added app and document components

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import 'assets/scss/main.scss';
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <style jsx global>{`
+        body {
+          font-family: 'Roboto', sans-serif;
+        }
+      `}</style>
+    </>
+  );
+}
