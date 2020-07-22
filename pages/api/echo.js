c90be319a70313ceaeb8578e4cee66447d118182
pB <webforeman@gmail.com>
2020-07-22 13:34:15 +0300

    created API

---
+++ pages/api/echo.js
@@ -0,0 +1,11 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function echo(req, res) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(
+    JSON.stringify({
+      message: req.query.message ?? 'Base message',
+    })
+  );
+}
